809b57f2bcffcf24a38c479f319b41f30b1931e6
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-02-02 22:13:13 +0400

    changing state

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,22 +2,32 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class Contact extends Component {
-  state = {};
+  state = {
+    showContactInfo: false
+  };
 
-  onShowClick = e => {};
+  onShowClick = () => {
+    this.setState({
+      showContactInfo: !this.state.showContactInfo
+    });
+  };
 
   render() {
+    const { showContactInfo } = this.state;
+
     const { name, email, phone } = this.props.contact;
     return (
       <div className="card card-body mb-3">
         <h4>
           {name}{" "}
-          <i onClick={() => this.onShowClick} className="fas fa-sort-down" />
+          <i onClick={() => this.onShowClick()} className="fas fa-sort-down" />
         </h4>
-        <ul className="list-group">
-          <li className="list-group-item">Email: {email}</li>
-          <li className="list-group-item">Phone: {phone}</li>
-        </ul>
+        {showContactInfo && (
+          <ul className="list-group">
+            <li className="list-group-item">Email: {email}</li>
+            <li className="list-group-item">Phone: {phone}</li>
+          </ul>
+        )}
       </div>
     );
   }
