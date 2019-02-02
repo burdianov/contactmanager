c8a30a5056d5661959ec94136101859942335ed8
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-02-02 13:24:18 +0400

    minor refactoring

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-function Header() {
+const Header = props => {
+  const { branding } = props.branding;
   return (
     <div>
-      <h1>Contact Manager</h1>
+      <h1>{branding}</h1>
     </div>
   );
-}
+};
 
 export default Header;
