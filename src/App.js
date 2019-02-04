012d279af9e53674e40f2b9b2cff4a1058063cdf
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-02-04 23:04:40 +0400

    create a form with state

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from "./components/layout/Header";
 import { Provider } from "./context";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import AddContact from "./components/contacts/AddContact";
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
         <div className="App">
           <Header branding="Contact Manager" />
           <div className="container">
+            <AddContact />
             <Contacts />
           </div>
         </div>
