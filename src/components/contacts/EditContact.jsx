7868951300bff831237eb80125527bf6ed487a29
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-02-09 21:07:38 +0400

    put request

--- src/components/contacts/EditContact.jsx
+++ src/components/contacts/EditContact.jsx
@@ -48,6 +48,21 @@ class EditContact extends Component {
       return;
     }
 
+    const updContact = {
+      name,
+      email,
+      phone
+    };
+
+    const { id } = this.props.match.params;
+
+    const res = await axios.put(
+      `https://jsonplaceholder.typicode.com/users/${id}`,
+      updContact
+    );
+
+    dispatch({ type: "UPDATE_CONTACT", payload: res.data });
+
     // clear state
     this.setState({
       name: "",
@@ -55,6 +70,7 @@ class EditContact extends Component {
       phone: "",
       errors: {}
     });
+
     this.props.history.push("/");
   };
 
